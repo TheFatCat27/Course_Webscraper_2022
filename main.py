814416961b3f39de2b40f383c5d3dcafbc046c56
Kayla Lattaway <kaylalattaway@gmail.com>
2023-01-07 22:15:47 +1300

    added dictionary to json and file created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from Scraper import dict_of_courses
 
@@ -16,7 +17,11 @@ def main():
     '''
     list_of_links = list(dict.fromkeys(list_of_links))
     dict_of_course = dict_of_courses(list_of_links)
-    print(dict_of_course)
+    print(*dict_of_course, sep="\n")
+
+    open("myfile.json", "w").close()
+    with open("myfile.json", "w") as f:
+        json.dump(dict_of_course, f, indent=6)
 
 if __name__ == "__main__":
     main()
